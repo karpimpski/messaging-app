a79231c23364ce7c07e80989b5ed2a68933ea0fc
Michael Karpinski <mike.karpinski99@gmail.com>
2017-01-14 23:27:08 -0500

    removed jQuery dependency

--- public/main.js
+++ public/main.js
@@ -1,40 +1,43 @@
 var socket = io();
 
+var userEl = document.getElementById('user');
+var name = makeid();
+var m = document.getElementById('m');
+var messages = document.getElementById('messages');
+var notifyUser = document.getElementById('notifyUser');
+
+userEl.value = name;
+socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion');
+
 document.getElementById('form').addEventListener('submit', function(e){
 	e.preventDefault();
-	var from = $('#user').val();
-	var message = $('#m').val();
+	var from = userEl.value;
+	var message = m.value;
 	if(message !== ''){
 		socket.emit('chatMessage', from, message);
 	}
-	$('#m').val('').focus();
+	m.value = '';
 	return false;
 })
 
 function notifyTyping(){
-	var user = $('#user').val();
+	var user = userEl.value;
 	socket.emit('notifyUser', user);
 }
 
 socket.on('chatMessage', function(from, msg){
-  var me = $('#user').val();
+  var me = userEl.value;
   var color = (from == me) ? 'green' : '#009afd';
   var from = (from == me) ? 'Me' : from;
-  $('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>');
+  messages.innerHTML += '<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>';
 });
 
 socket.on('notifyUser', function(user){
-  var me = $('#user').val();
+  var me = userEl.value;
   if(user != me) {
-    $('#notifyUser').text(user + ' is typing ...');
+    notifyUser.innerHTML = user + ' is typing ...';
   }
-  setTimeout(function(){ $('#notifyUser').text(''); }, 10000);;
-});
-
-$(document).ready(function(){
-  var name = makeid();
-  $('#user').val(name);
-  socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion');
+  setTimeout(function(){ notifyUser.innerHTML = ''; }, 10000);;
 });
 
 function makeid() {
