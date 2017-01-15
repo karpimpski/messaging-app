76555d9ba623884069acc69530387b4c9e4cdef9
Michael Karpinski <mike.karpinski99@gmail.com>
2017-01-14 23:01:02 -0500

    basic chat app

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.get('/', function(req, res){
+	app.use(express.static(__dirname + '/public'));
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+	socket.on('chatMessage', function(from, msg){
+		io.emit('chatMessage', from, msg);
+	});
+
+	socket.on('notifyUser', function(user){
+		io.emit('notifyUser', user);
+	});
+})
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
