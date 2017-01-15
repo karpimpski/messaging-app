9a7021b0459a09f46440158b6c0b7a1f18e724b5
Michael Karpinski <mike.karpinski99@gmail.com>
2017-01-15 02:31:44 -0500

    user validation

--- public/main.js
+++ public/main.js
@@ -1,13 +1,36 @@
-var socket = io();
+var socket;
 
 var userEl = document.getElementById('user');
-var name = makeid();
 var m = document.getElementById('m');
 var messages = document.getElementById('messages');
 var notifyUser = document.getElementById('notifyUser');
 
-userEl.value = name;
-socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion');
+
+
+document.getElementById('username').addEventListener('submit', function(e){
+  e.preventDefault();
+  var name = document.getElementById('name').value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var res = JSON.parse(xhttp.responseText);
+      if(res == null){
+        enter(name);
+      }
+    }
+  };
+  xhttp.open("GET", "/api/user/" + name, true);
+  xhttp.send();
+});
+
+function enter(name){
+  document.getElementById('login').classList.add('hidden');
+  document.getElementById('chat').classList.remove('hidden');
+  userEl.value = name;
+  socket = io();
+  socket.emit('newUser', name);
+  setSocketListeners();
+}
 
 document.getElementById('form').addEventListener('submit', function(e){
 	e.preventDefault();
@@ -18,33 +41,37 @@ document.getElementById('form').addEventListener('submit', function(e){
 	}
 	m.value = '';
 	return false;
-})
+});
+
+function setSocketListeners(){
+  socket.on('chatMessage', function(from, msg){
+    var me = userEl.value;
+    var color = (from == me) ? 'green' : '#009afd';
+    var from = (from == me) ? 'Me' : from;
+    messages.innerHTML += '<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>';
+  });
+
+  socket.on('notifyUser', function(user){
+    var me = userEl.value;
+    if(user != me) {
+      notifyUser.innerHTML = user + ' is typing ...';
+      notifyUser.classList.add('active');
+    }
+    setTimeout(function(){ 
+      notifyUser.classList.remove('active');
+      setTimeout(function(){
+        notifyUser.innerHTML = '';
+      }, 500)
+    }, 10000);;
+  });
+}
 
 function notifyTyping(){
 	var user = userEl.value;
 	socket.emit('notifyUser', user);
 }
 
-socket.on('chatMessage', function(from, msg){
-  var me = userEl.value;
-  var color = (from == me) ? 'green' : '#009afd';
-  var from = (from == me) ? 'Me' : from;
-  messages.innerHTML += '<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>';
-});
 
-socket.on('notifyUser', function(user){
-  var me = userEl.value;
-  if(user != me) {
-    notifyUser.innerHTML = user + ' is typing ...';
-    notifyUser.classList.add('active');
-  }
-  setTimeout(function(){ 
-  	notifyUser.classList.remove('active');
-  	setTimeout(function(){
-  		notifyUser.innerHTML = '';
-  	}, 500)
-  }, 10000);;
-});
 
 function makeid() {
   var text = "";
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-var clients = [];
+var mongo = require('mongodb').MongoClient;
+var url = process.env.DB_URI;
 
 app.get('/', function(req, res){
 	app.use(express.static(__dirname + '/public'));
@@ -11,6 +11,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+	console.log('user connected');
+	socket.on('newUser', function(name){
+		mongo.connect(url, function(err, db){
+			db.collection('users').insert({name: name});
+		});
+	});
+
 	socket.on('chatMessage', function(from, msg){
 		io.emit('chatMessage', from, msg);
 	});
@@ -20,9 +27,20 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('disconnect', function(){
+		mongo.connect(url, function(err, db){
+			db.collection('users').remove({name: name});
+		})
 		console.log(socket + ' disconnected');
-	})
-})
+	});
+});
+
+app.get('/api/user/:user', function(req, res){
+	mongo.connect(url, function(err, db){
+		db.collection('users').findOne({name: req.params.user}, function(err, doc){
+			res.end(JSON.stringify(doc));
+		});
+	});
+});
 
 http.listen(process.env.PORT, function(){
   
